aefe48c132c7203b38186f8b4a36a2a410eecf21
gopinathsivaraman <gopinathsivaraman@live.com>
2017-11-23 01:28:46 +0530

    converting hex to rgb functionality added

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,45 @@ $(document).ready(function(){
 		}
 		
 		if( lengthOfHexField == 4 || lengthOfHexField == 7 ){
-			//rgbOfHex = getRGBOfHex( valOfHexField.substring(1,) );
+			rgbOfHex = getRGBOfHex( valOfHexField.substring(1,) );
 			
 		}else{
 			$('body').css('background-color', '#a5b');
+			$('#rgb').val("");
 		}
 	});
 });
 
 function getRGBOfHex(hex){
 	var hexLength = hex.length;
+	
+	if( isHexValid(hex) ){
+		
+		if( hexLength == 3){
+			red = ( parseInt(hex[0],16) * 16 ) + ( parseInt( hex[0], 16 ) * 1 );
+			green = ( parseInt(hex[1],16) * 16 ) + ( parseInt( hex[1], 16 ) * 1 );
+			blue = ( parseInt(hex[2],16) * 16 ) + ( parseInt( hex[2], 16 ) * 1 );
+		}else if( hexLength == 6 ){
+			red = ( parseInt(hex[0],16) * 16 ) + ( parseInt( hex[1], 16 ) * 1 );
+			green = ( parseInt(hex[2],16) * 16 ) + ( parseInt( hex[3], 16 ) * 1 );
+			blue = ( parseInt(hex[4],16) * 16 ) + ( parseInt( hex[5], 16 ) * 1 );
+		}
+		
+		$('body').css('background-color', '#'+hex);
+		$('#rgb').val('rgb('+red+','+green+','+blue+')');
+		
+	}
+}
+
+function isHexValid(hex){
+	isValid = true;
+	
+	for(i=0; i<hex.length; i++){
+		if( isNaN(parseInt(hex[i],16)) ){
+			isValid = false;
+			break;
+		}
+	}
+	
+	return isValid;
 }
